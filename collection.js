db7a5734c3c503a60ff4d444e6985b443503296a
eya zayeni <ayoutabechik555@gmail.com>
2023-04-12 08:31:23 +0200

    labface

--- collection.js
+++ collection.js
@@ -92,19 +92,19 @@ let products = [
     },
     {
         id: 4,
-        name: 'painted short',
+        name: 'Painted Short',
         image: '4.JPG',
         price: 123000
     },
     {
         id: 5,
-        name: 'PRODUCT NAME 5',
+        name: 'Painted Jean',
         image: '5.JPG',
         price: 320000
     },
     {
         id: 6,
-        name: 'PRODUCT NAME 6',
+        name: 'painted jean',
         image: '6.JPG',
         price: 120000
     }
@@ -169,9 +169,9 @@ function changeQuantity(key, quantity){
 
 
 
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+var current_fs, next_fs, previous_fs; 
+var left, opacity, scale; 
+var animating; 
 
 $(".next").click(function(){
 	if(animating) return false;
@@ -180,20 +180,19 @@ $(".next").click(function(){
 	current_fs = $(this).parent();
 	next_fs = $(this).parent().next();
 	
-	//activate next step on progressbar using the index of next_fs
+	
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 	
-	//show the next fieldset
+
 	next_fs.show(); 
-	//hide the current fieldset with style
+	
 	current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
+			
 			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
+			
 			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
+			
 			opacity = 1 - now;
 			current_fs.css({
         'transform': 'scale('+scale+')',
@@ -206,7 +205,7 @@ $(".next").click(function(){
 			current_fs.hide();
 			animating = false;
 		}, 
-		//this comes from the custom easing plugin
+		
 		easing: 'easeInOutBack'
 	});
 });
@@ -218,20 +217,19 @@ $(".previous").click(function(){
 	current_fs = $(this).parent();
 	previous_fs = $(this).parent().prev();
 	
-	//de-activate current step on progressbar
+	
 	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
 	
-	//show the previous fieldset
+	
 	previous_fs.show(); 
-	//hide the current fieldset with style
+	
 	current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
+			
 			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
+			
 			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
+			
 			opacity = 1 - now;
 			current_fs.css({'left': left});
 			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
@@ -262,7 +260,7 @@ function updateTshirtImage(imageURL) {
   });
 }
 
-// Update the TShirt color according to the selected color by the user
+
 document.getElementById("tshirt-color").addEventListener(
   "change",
   function () {
@@ -271,12 +269,11 @@ document.getElementById("tshirt-color").addEventListener(
   false
 );
 
-// Update the TShirt color according to the selected color by the user
+
 document.getElementById("tshirt-design").addEventListener(
   "change",
   function () {
-    // Call the updateTshirtImage method providing as first argument the URL
-    // of the image provided by the select
+   
     updateTshirtImage(this.value);
   },
   false
